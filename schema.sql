58e52c8f98bd97e1ffe59129028a0df28ad6f21d
unknown <brenomoraes97@yahoo.com>
2023-02-27 21:53:47 -0300

    feat: populating categories table

--- schema.sql
+++ schema.sql
@@ -22,3 +22,22 @@ CREATE TABLE IF NOT EXISTS transactions (
   user_id INTEGER NOT NULL REFERENCES users(id),
   type TEXT
   );
+
+INSERT INTO categories (description) VALUES
+('Alimentação'),
+('Assinaturas e Serviços'),
+('Casa'),
+('Mercado'),
+('Cuidados Pessoais'),
+('Educação'),
+('Família'),
+('Lazer'),
+('Pets'),
+('Presentes'),
+('Roupas'),
+('Saúde'),
+('Transporte'),
+('Salário'),
+('Vendas'),
+('Outras receitas'),
+('Outras despesas');
