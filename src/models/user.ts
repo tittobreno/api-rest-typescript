55742a59675557b41c0c6c0895e27269592c7ad0
unknown <brenomoraes97@yahoo.com>
2023-03-06 22:10:21 -0300

    feat: created transaction model

--- src/models/user.ts
+++ src/models/user.ts
@@ -4,3 +4,12 @@ export interface UserModel {
   email: string;
   password: string;
 }
+
+export interface TransactionModel {
+  description: string;
+  value: number;
+  date: Date;
+  category_id: number;
+  user_id: number;
+  type: string;
+}
