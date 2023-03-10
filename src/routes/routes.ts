cc3d495519929a4be297cdb5a6304ad70410c212
unknown <brenomoraes97@yahoo.com>
2023-03-10 02:56:44 -0300

    style: reordering imports

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,12 +1,11 @@
-import { Response, Router } from "express";
+import { Router } from "express";
 import getCategories from "../controllers/categories";
-import { createUser, signIn } from "../controllers/user";
-import authenticateUser from "../middlewares/userAuthenticator";
 import {
   createTransaction,
   listTransactions,
 } from "../controllers/transactions";
-import { MyReq } from "../types";
+import { createUser, signIn } from "../controllers/user";
+import authenticateUser from "../middlewares/userAuthenticator";
 const router = Router();
 
 router.post("/cadastrar", createUser);
@@ -19,8 +18,4 @@ router.get("/categorias", getCategories);
 router.get("/transacao/listar", listTransactions);
 router.post("/transacao/cadastrar", createTransaction);
 
-router.get("/test", (req: MyReq, res: Response) => {
-  const user = req.userData;
-  return res.json(user);
-});
 export default router;
