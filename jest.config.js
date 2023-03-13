9998bbad675a8dc60f3cf6464d066c72b47a3512
unknown <brenomoraes97@yahoo.com>
2023-03-13 16:39:15 -0300

    test: added and configured jest to project

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/__tests__'],
+  testMatch: ['**/*.test.(ts|js)'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'json'],
+};
