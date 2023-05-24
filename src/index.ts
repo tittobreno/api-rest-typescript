766f1372e8efda22c7ee5c69086697638a91ebc3
unknown <brenomoraes97@yahoo.com>
2023-05-23 23:42:02 -0300

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import router from "./routes/routes";
+import cors from "cors";
 require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use(router);
 
 app.listen(process.env.PORT, () => {
